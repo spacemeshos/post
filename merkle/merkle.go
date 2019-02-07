52c75cc042ade5b1c46d468def4074850d8314c8
Noam Nelke <noamnelke@gmail.com>
2019-02-07 22:21:52 +0200

    generate merkle proofs

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -1,6 +1,8 @@
 package merkle
 
 import (
+	"encoding/hex"
+	"fmt"
 	"github.com/spacemeshos/sha256-simd"
 	"math"
 	"post-private/datatypes"
@@ -9,28 +11,60 @@ import (
 type Tree interface {
 	AddLeaf(label datatypes.Label)
 	Root() []byte
+	Proof() []node
 }
 
 type node []byte
 
+func (n node) String() string {
+	return hex.EncodeToString(n)[:4]
+}
+
 type incrementalTree struct {
-	path []node
+	path        []node
+	currentLeaf uint64
+	leafToProve *uint64
+	proof       []node
+	nodes       [][]node // TODO @noam: Remove!
 }
 
 func NewTree(width uint64) Tree {
-	return &incrementalTree{path: make([]node, int(math.Log2(float64(width)))+1)}
+	depth := int(math.Log2(float64(width))) + 1
+	return &incrementalTree{
+		path:        make([]node, depth),
+		currentLeaf: 0,
+		leafToProve: nil,
+		proof:       nil,
+		nodes:       make([][]node, depth, width), // TODO @noam: Remove!
+	}
 }
 
-func (t incrementalTree) AddLeaf(label datatypes.Label) {
+func NewTreeWithProof(width, leafToProve uint64) Tree {
+	depth := int(math.Log2(float64(width))) + 1
+	return &incrementalTree{
+		path:        make([]node, depth),
+		currentLeaf: 0,
+		leafToProve: &leafToProve,
+		proof:       make([]node, depth-1),
+		nodes:       make([][]node, depth, width), // TODO @noam: Remove!
+	}
+}
+
+func (t *incrementalTree) AddLeaf(label datatypes.Label) {
 	activeNode := node(label)
-	for i, n := range t.path {
-		if n == nil {
+	for i := range t.path {
+		t.nodes[i] = append(t.nodes[i], activeNode) // TODO @noam: Remove!
+		if t.isNodeInProof(uint(i)) {
+			t.proof[i] = activeNode
+		}
+		if t.path[i] == nil {
 			t.path[i] = activeNode
 			break
 		}
-		activeNode = sum(n, activeNode)
+		activeNode = sum(t.path[i], activeNode)
 		t.path[i] = nil
 	}
+	t.currentLeaf++
 }
 
 func sum(left node, right node) node {
@@ -38,6 +72,44 @@ func sum(left node, right node) node {
 	return res[:]
 }
 
-func (t incrementalTree) Root() []byte {
+func (t *incrementalTree) Root() []byte {
 	return t.path[len(t.path)-1]
+}
+
+func (t *incrementalTree) Proof() []node {
+	printTree(t.nodes) // TODO @noam: Remove!
+	return t.proof
+}
+
+func (t *incrementalTree) isNodeInProof(layer uint) bool {
+	if t.leafToProve == nil {
+		return false
+	}
+
+	var bottomMask uint64 = 1<<(layer+1) - 1 // layer 2 (third layer) becomes ...000111
+	var currMask uint64 = 1 << layer // layer 2 (third layer) becomes ...000100
+
+	samePathAboveCurrentLayer := (t.currentLeaf^*t.leafToProve)|bottomMask == bottomMask
+	differentAtCurrentLayer := (t.currentLeaf^*t.leafToProve)&currMask == currMask
+
+	return samePathAboveCurrentLayer && differentAtCurrentLayer
+
+	/* Explanation:
+
+	The index in binary form (most- to least-significant) represents the path from the top (root) of the tree to the
+	bottom.
+	When we mask the bottom of the tree (`layer` least significant binary digits) we require the currently added leaf's
+	index and the proved leaf index to be identical, in other words - they share the path from the root (top) of the
+	tree to the current layer.
+	We also require that the current layer is different - so the currently handled node is a sibling of one of the nodes
+	in the path to the proved node -- so we want it in the proof.
+
+	 */
+}
+
+// TODO @noam: Remove!
+func printTree(nodes [][]node) {
+	for _, n := range nodes {
+		defer fmt.Println(n)
+	}
 }
--- merkle/merkle_test.go
+++ merkle/merkle_test.go
@@ -30,3 +30,19 @@ func _TestNewTreeBig(t *testing.T) {
 	--- PASS: TestNewTreeBig (11.90s)
 	 */
 }
+
+func TestNewTreeWithProof(t *testing.T) {
+	tree := NewTreeWithProof(8, 4)
+	for i := 0; i < 8; i++ {
+		tree.AddLeaf(datatypes.NewLabel(uint64(i)))
+	}
+	expectedRoot, _ := hex.DecodeString("4a2ca61d1fd537170785a8575d424634713c82e7392e67795a807653e498cfd0")
+	require.Equal(t, expectedRoot, tree.Root())
+
+	expectedProof := make([]node, 3)
+	expectedProof[0], _ = hex.DecodeString("0500000000000000")
+	expectedProof[1], _ = hex.DecodeString("6b2e10cb2111114ce942174c38e7ea38864cc364a8fe95c66869c85888d812da")
+	expectedProof[2], _ = hex.DecodeString("13c04a6157aa640f711d230a4f04bc2b19e75df1127dfc899f025f3aa282912d")
+
+	require.EqualValues(t, expectedProof, tree.Proof())
+}
