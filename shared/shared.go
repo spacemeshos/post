ac2d8d7d40b8ca529255f9791e59e7aefd9e3f2b
Moshe Shababo <dicentralized@gmail.com>
2019-06-06 08:06:14 +0300

    remove consts file

--- shared/shared.go
+++ shared/shared.go
@@ -2,6 +2,22 @@ package shared
 
 import (
 	"errors"
+	"github.com/spacemeshos/merkle-tree"
+	"math"
+)
+
+const (
+	LabelGroupSize = merkle.NodeSize
+
+	// In bytes. 1 peta-byte of storage.
+	// This would protect against number of labels uint64 overflow as well,
+	// since the number of labels per byte can be 8 at most (3 extra bit shifts).
+	MaxSpace = 1 << 40 // 1099511627777
+
+	MaxNumOfFiles = math.MaxUint8 // 255
+
+	MinDifficulty = 5 // 1 byte per label
+	MaxDifficulty = 8 // 1 bit per label
 )
 
 var (
