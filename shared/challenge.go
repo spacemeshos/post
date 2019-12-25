dea2180a3920dfb9f9573c5762e806b342c769b6
Noam Nelke <noam@spacemesh.io>
2019-12-25 15:18:17 +0200

    Less allocations in GetSha256Parent (#34)

--- shared/challenge.go
+++ shared/challenge.go
@@ -4,10 +4,20 @@ import "github.com/spacemeshos/sha256-simd"
 
 type Challenge []byte
 
-var ZeroChallenge = make(Challenge, 0)
+var (
+	ZeroChallenge = make(Challenge, 0)
+	buffer        []byte
+)
 
+// ⚠️ This method is NOT thread-safe. The code is optimized for performance and memory allocations.
 func (ch Challenge) GetSha256Parent(lChild, rChild []byte) []byte {
-	children := append(lChild, rChild...)
-	res := sha256.Sum256(append(ch, children...))
+	l := len(ch) + len(lChild) + len(rChild)
+	if len(buffer) < l {
+		buffer = make([]byte, l)
+	}
+	copy(buffer, ch)
+	copy(buffer[len(ch):], lChild)
+	copy(buffer[len(ch)+len(lChild):], rChild)
+	res := sha256.Sum256(buffer[:l])
 	return res[:]
 }
