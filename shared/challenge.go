a2835f1140532102990908bf31acf0831af94866
Noam Nelke <noamnelke@gmail.com>
2019-12-25 09:47:20 +0200

    less allocations in GetSha256Parent

--- shared/challenge.go
+++ shared/challenge.go
@@ -4,10 +4,19 @@ import "github.com/spacemeshos/sha256-simd"
 
 type Challenge []byte
 
-var ZeroChallenge = make(Challenge, 0)
+var (
+	ZeroChallenge = make(Challenge, 0)
+	message []byte
+)
 
+// ⚠️ This method is NOT thread-safe
 func (ch Challenge) GetSha256Parent(lChild, rChild []byte) []byte {
-	children := append(lChild, rChild...)
-	res := sha256.Sum256(append(ch, children...))
+	if len(message) != len(ch)+len(lChild)+len(rChild) {
+		message = make([]byte, len(ch)+len(lChild)+len(rChild))
+	}
+	copy(message, ch)
+	copy(message[len(ch):], lChild)
+	copy(message[len(ch)+len(lChild):], rChild)
+	res := sha256.Sum256(message)
 	return res[:]
 }
