f6e2140b7f6e7c9ca5fdfbde90d7fe3554ee843d
Moshe Shababo <dicentralized@gmail.com>
2021-05-09 11:27:00 +0300

    temp disable rpc server

--- post.go
+++ post.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"github.com/spacemeshos/post/cmd/server"
-	"os"
-)
-
 func main() {
-	if err := server.Cmd.Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	//if err := server.Cmd.Execute(); err != nil {
+	//	_, _ = fmt.Fprintln(os.Stderr, err)
+	//	os.Exit(1)
+	//}
 }
