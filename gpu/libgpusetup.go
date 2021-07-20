cb407b43bd26adb7642fefe40ea0a37a0bc83b1e
Alexey Sudachen <alexey@sudachen.name>
2021-07-20 17:09:35 -0400

    fixed rpath for osx

--- gpu/libgpusetup.go
+++ gpu/libgpusetup.go
@@ -1,7 +1,7 @@
 package gpu
 
 // #cgo linux LDFLAGS: -Wl,-rpath,$ORIGIN
-// #cgo darwin LDFLAGS: -Wl,-rpath,@load_path
+// #cgo darwin LDFLAGS: -Wl,-rpath -Wl,@loader_path
 // #cgo !windows LDFLAGS: -lgpu-setup
 // #cgo windows LDFLAGS: -L${SRCDIR} -lgpu-setup-win64
 import "C"
