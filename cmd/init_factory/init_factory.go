2ae779ba1a632d8b66caa3bddec19fb17f67d80a
Matthias <5011972+fasmat@users.noreply.github.com>
2022-11-24 17:58:45 +0000

    Fix some TODOs

--- cmd/init_factory/init_factory.go
+++ cmd/init_factory/init_factory.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"context"
-	"crypto/ed25519"
 	"encoding/hex"
 	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+
+	"github.com/minio/sha256-simd"
+
+	"github.com/spacemeshos/ed25519"
 	"github.com/spacemeshos/post/config"
 	"github.com/spacemeshos/post/initialization"
 	"github.com/spacemeshos/post/proving"
 	"github.com/spacemeshos/post/shared"
 	"github.com/spacemeshos/post/verifying"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
 )
 
 var (
@@ -23,39 +27,58 @@ var (
 	reset bool
 )
 
-func init() {
+func parseFlags() error {
 	flag.StringVar(&opts.DataDir, "datadir", opts.DataDir, "filesystem datadir path")
-	//flag.Uint64(&opts.NumUnits, "numunits", opts.NumUnits, "number of units") // TODO: workaround the missing type support for uint32
-	// TODO: expose more cfg/opts to cmd flags
+
+	var numUnits uint64
+	flag.Uint64Var(&numUnits, "numUnits", uint64(opts.NumUnits), "number of units") // workaround the missing type support for uint32
+	opts.NumUnits = uint32(numUnits)
+
 	flag.BoolVar(&reset, "reset", false, "whether to reset the datadir before starting")
 	idHex := flag.String("id", "", "id (public key) in hex")
+
+	// TODO: expose more cfg/opts to cmd flags
 	flag.Parse()
 
-	if *idHex == "" {
-		pub, priv, err := ed25519.GenerateKey(nil) // TODO: verify whether this is the current key generator.
-		if err != nil {
-			log.Fatalf("generate key error: %v", err)
-		}
-		log.Printf("generated id: %x", id)
-		saveKey(priv) // The key will need to be loaded in clients for the data to be usable.
-		id = pub
-	} else {
+	if *idHex != "" {
 		var err error
 		id, err = hex.DecodeString(*idHex)
 		if err != nil {
-			log.Fatalf("id hex decode error: %v", err)
+			return fmt.Errorf("invalid id: %w", err)
 		}
+		return nil
 	}
+
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return fmt.Errorf("failed to generate identity: %w", err)
+	}
+
+	id = pub
+	log.Printf("generated id: %x\n", id)
+
+	return saveKey(priv) // The key will need to be loaded in clients for the data to be usable.
+}
+
+// TODO(mafa): add "WithId" and "WithCommitmentATX" options to the initializer and do this within the initializer.
+func GetCommitmentBytes(id []byte, commitmentAtxId []byte) []byte {
+	h := sha256.Sum256(append(id, commitmentAtxId...))
+	return h[:]
 }
 
 func main() {
-	commitment := id // TODO: expose commitmentATX or commitment hash (commitmentATX ++ id) to cmd flags.
+	if err := parseFlags(); err != nil {
+		log.Fatalf("failed to parse flags: %v", err)
+	}
+
+	atxId := make([]byte, 32) // TODO(mafa): get this as a flag like the id.
+	commitment := GetCommitmentBytes(id, atxId)
 
 	init, err := initialization.NewInitializer(
 		initialization.WithConfig(cfg),
 		initialization.WithInitOpts(opts),
 		initialization.WithCommitment(commitment),
-		//initialization.WithLogger() // TODO: set a custom logger
+		//initialization.WithLogger() // TODO: add wrapper for zap logger.
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -67,7 +90,10 @@ func main() {
 		}
 	}
 
-	if err := init.Initialize(context.TODO()); err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	if err := init.Initialize(ctx); err != nil {
 		if err == shared.ErrInitCompleted {
 			log.Print(err)
 			return
@@ -80,24 +106,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// prover.SetLogger() // TODO: set a custom logger
+	// prover.SetLogger() // TODO: add wrapper for zap logger.
 	proof, proofMetadata, err := prover.GenerateProof(shared.ZeroChallenge)
 	if err != nil {
 		log.Fatalf("proof generation error: %v", err)
 	}
 	if err := verifying.Verify(proof, proofMetadata); err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to verify test proof: %v", err)
 	}
 }
 
-func saveKey(key []byte) {
-	err := os.MkdirAll(opts.DataDir, shared.OwnerReadWriteExec)
-	if err != nil && !os.IsExist(err) {
-		log.Fatalf("mkdir error: %v", err)
+func saveKey(key []byte) error {
+	if err := os.MkdirAll(opts.DataDir, shared.OwnerReadWriteExec); err != nil && !os.IsExist(err) {
+		return fmt.Errorf("mkdir error: %w", err)
 	}
 
-	err = ioutil.WriteFile(filepath.Join(opts.DataDir, "key.bin"), key, shared.OwnerReadWrite)
-	if err != nil {
-		log.Fatalf("key write to disk error: %v", err)
+	if err := os.WriteFile(filepath.Join(opts.DataDir, "key.bin"), key, shared.OwnerReadWrite); err != nil {
+		return fmt.Errorf("key write to disk error: %w", err)
 	}
+	return nil
 }
