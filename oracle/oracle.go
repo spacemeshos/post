cae627022b64f5d16c44ab19a00ce4534ec0e002
Matthias <5011972+fasmat@users.noreply.github.com>
2022-11-25 10:11:02 +0000

    Add more functional options and documentation for WorkOracle

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/minio/sha256-simd"
 
+	"github.com/spacemeshos/go-spacemesh/hash"
 	"github.com/spacemeshos/post/config"
 	"github.com/spacemeshos/post/gpu"
 	"github.com/spacemeshos/post/shared"
@@ -13,11 +14,13 @@ import (
 
 type Challenge = shared.Challenge
 
-type workOracleOption struct {
+type option struct {
 	computeProviderID uint
 
-	commitment []byte
-	salt       []byte
+	nodeId []byte
+	atxId  []byte
+
+	salt []byte
 
 	startPosition uint64
 	endPosition   uint64
@@ -28,44 +31,89 @@ type workOracleOption struct {
 	difficulty    []byte
 }
 
-type workOracleOptionFunc func(*workOracleOption) error
+func (o *option) validate() error {
+	if o.nodeId == nil {
+		return fmt.Errorf("`nodeId` is required")
+	}
 
-func WithComputeProviderID(id uint) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+	if o.atxId == nil {
+		return fmt.Errorf("`atxId` is required")
+	}
+
+	if o.bitsPerLabel == 0 {
+		return fmt.Errorf("`bitsPerLabel` is required")
+	}
+
+	return nil
+}
+
+type OptionFunc func(*option) error
+
+// WithComputeProviderID sets the ID of the compute provider to use.
+func WithComputeProviderID(id uint) OptionFunc {
+	return func(opts *option) error {
 		opts.computeProviderID = id
 		return nil
 	}
 }
 
-func WithCommitment(commitment []byte) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
-		if len(commitment) != 32 {
-			return fmt.Errorf("invalid `commitment` length; expected: 32, given: %v", len(commitment))
+// WithNodeId sets the ID of the Node.
+func WithNodeId(nodeId []byte) OptionFunc {
+	return func(opts *option) error {
+		if len(nodeId) != 32 {
+			return fmt.Errorf("invalid `id` length; expected: 32, given: %v", len(nodeId))
 		}
 
-		opts.commitment = commitment
+		opts.nodeId = nodeId
 		return nil
 	}
 }
 
-func WithPosition(position uint64) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+// WithAtxId sets the ID of the CommitmentATX.
+func WithAtxId(atxId []byte) OptionFunc {
+	return func(opts *option) error {
+		if len(atxId) != 32 {
+			return fmt.Errorf("invalid `commitmentAtx` length; expected: 32, given: %v", len(atxId))
+		}
+
+		opts.atxId = atxId
+		return nil
+	}
+}
+
+// WithSalt sets the salt to use for the oracle.
+func WithSalt(salt []byte) OptionFunc {
+	return func(opts *option) error {
+		if len(salt) != 32 {
+			return fmt.Errorf("invalid `salt` length; expected: 32, given: %v", len(salt))
+		}
+
+		opts.salt = salt
+		return nil
+	}
+}
+
+// WithPosition sets the index of one label to compute.
+func WithPosition(position uint64) OptionFunc {
+	return func(opts *option) error {
 		opts.startPosition = position
 		opts.endPosition = position
 		return nil
 	}
 }
 
-func WithStartAndEndPosition(start, end uint64) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+// WithStartAndEndPosition sets the range of indices of labels for the oracle to compute.
+func WithStartAndEndPosition(start, end uint64) OptionFunc {
+	return func(opts *option) error {
 		opts.startPosition = start
 		opts.endPosition = end
 		return nil
 	}
 }
 
-func WithBitsPerLabel(bitsPerLabel uint32) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+// WithBitsPerLabel sets the number of bits per label.
+func WithBitsPerLabel(bitsPerLabel uint32) OptionFunc {
+	return func(opts *option) error {
 		if bitsPerLabel < config.MinBitsPerLabel || bitsPerLabel > config.MaxBitsPerLabel {
 			return fmt.Errorf("invalid `bitsPerLabel`; expected: %d-%d, given: %v", config.MinBitsPerLabel, config.MaxBitsPerLabel, bitsPerLabel)
 		}
@@ -77,8 +125,8 @@ func WithBitsPerLabel(bitsPerLabel uint32) workOracleOptionFunc {
 // WithComputeLeaves instructs the oracle to compute the labels for PoST or not.
 // By default computing leaves is enabled. It can be switched off to save time
 // when continuing a run to compute a proof of work.
-func WithComputeLeaves(enabled bool) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+func WithComputeLeaves(enabled bool) OptionFunc {
+	return func(opts *option) error {
 		opts.computeLeaves = enabled
 		return nil
 	}
@@ -88,8 +136,8 @@ func WithComputeLeaves(enabled bool) workOracleOptionFunc {
 // If difficulty is nil, no PoW will be computed. Otherwise it specifies the difficulty
 // of the PoW to be computed (higher values are more difficult).
 // By default computing proof of work is disabled.
-func WithComputePow(difficulty []byte) workOracleOptionFunc {
-	return func(opts *workOracleOption) error {
+func WithComputePow(difficulty []byte) OptionFunc {
+	return func(opts *option) error {
 		if difficulty != nil && len(difficulty) != 32 {
 			return fmt.Errorf("invalid `difficulty` length; expected: 32, given: %v", len(difficulty))
 		}
@@ -99,13 +147,17 @@ func WithComputePow(difficulty []byte) workOracleOptionFunc {
 	}
 }
 
+// WorkOracleResult is the result of a call to WorkOracle.
+// It contains the computed labels and the nonce as a proof of work.
 type WorkOracleResult struct {
-	Output []byte
-	Nonce  *uint64
+	Output []byte  // Output are the computed labels (only if `WithComputeLeaves` is true - default yes).
+	Nonce  *uint64 // Nonce is the nonce of the proof of work (only if `WithComputePow` is true - default no).
 }
 
-func WorkOracle(opts ...workOracleOptionFunc) (WorkOracleResult, error) {
-	options := &workOracleOption{
+// WorkOracle computes labels for a given challenge for a Node with the provided CommitmentATX ID.
+// The labels are computed using the specified compute provider (default: CPU).
+func WorkOracle(opts ...OptionFunc) (WorkOracleResult, error) {
+	options := &option{
 		computeProviderID: gpu.CPUProviderID(),
 		salt:              make([]byte, 32), // TODO(moshababo): apply salt
 		computeLeaves:     true,
@@ -117,9 +169,15 @@ func WorkOracle(opts ...workOracleOptionFunc) (WorkOracleResult, error) {
 		}
 	}
 
+	if err := options.validate(); err != nil {
+		return WorkOracleResult{}, err
+	}
+
+	commitment := hash.Sum(append(options.nodeId, options.atxId...))
+
 	res, err := gpu.ScryptPositions(
 		gpu.WithComputeProviderID(options.computeProviderID),
-		gpu.WithCommitment(options.commitment),
+		gpu.WithCommitment(commitment[:]),
 		gpu.WithSalt(options.salt),
 		gpu.WithStartAndEndPosition(options.startPosition, options.endPosition),
 		gpu.WithBitsPerLabel(options.bitsPerLabel),
