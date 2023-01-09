5d7e322d183f38d2780edf9d529c0004d48c5fc8
Moshe Shababo <dicentralized@gmail.com>
2023-01-09 18:27:57 +0200

    Add postcli as an additional executable (#96)
    * init_factory, WIP
    
    * Fix some TODOs
    
    * Don't print error when ctrl+c out of the factory
    
    * Add makefile target for cli
    
    * Finalize postcli
    
    * Fix missing import
    
    * Fix preemptive flags
    
    * Remove -reset from example
    
    * Fix -printConfig
    
    * Add missing dependencies
    
    * Fix compile issues in postcli
    
    * Use any instead of interface{}
    
    * Add ed25519 dep
    
    * * fix -numUnits bug
    * add -maxFileSize
    * add -genProof
    * return after reset
    
    * Update cmd/postcli/main.go
    
    Co-authored-by: Matthias Fasching <5011972+fasmat@users.noreply.github.com>
    
    * gci fix
    
    * golangci-lint fix
    
    * make tidy
    
    * Update go.mod
    
    Co-authored-by: Matthias <5011972+fasmat@users.noreply.github.com>

---
+++ cmd/postcli/main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"context"
+	"encoding/hex"
+	"errors"
+	"flag"
+	"fmt"
+	baseLog "log"
+	"os"
+	"os/signal"
+	"path/filepath"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/spacemeshos/ed25519"
+
+	"github.com/spacemeshos/post/config"
+	"github.com/spacemeshos/post/gpu"
+	"github.com/spacemeshos/post/initialization"
+	"github.com/spacemeshos/post/proving"
+	"github.com/spacemeshos/post/shared"
+	"github.com/spacemeshos/post/verifying"
+)
+
+var (
+	cfg                = config.DefaultConfig()
+	opts               = config.DefaultInitOpts()
+	log                = logger{}
+	printProviders     bool
+	printConfig        bool
+	genProof           bool
+	idHex              string
+	id                 []byte
+	commitmentAtxIdHex string
+	commitmentAtxId    []byte
+	reset              bool
+)
+
+func parseFlags() {
+	flag.BoolVar(&printProviders, "printProviders", false, "print the list of compute providers")
+	flag.BoolVar(&printConfig, "printConfig", false, "print the used config and options")
+	flag.BoolVar(&genProof, "genproof", false, "generate proof as a sanity test, after initialization")
+	flag.StringVar(&opts.DataDir, "datadir", opts.DataDir, "filesystem datadir path")
+	flag.Uint64Var(&opts.MaxFileSize, "maxFileSize", opts.MaxFileSize, "max file size")
+	flag.IntVar(&opts.ComputeProviderID, "provider", opts.ComputeProviderID, "compute provider id (required)")
+	flag.Uint64Var(&cfg.LabelsPerUnit, "labelsPerUnit", cfg.LabelsPerUnit, "the number of labels per unit")
+	flag.BoolVar(&reset, "reset", false, "whether to reset the datadir before starting")
+	flag.StringVar(&idHex, "id", "", "miner's id (public key), in hex (will be auto-generated if not provided)")
+	flag.StringVar(&commitmentAtxIdHex, "commitmentAtxId", "", "commitment atx id, in hex (required)")
+	numUnits := flag.Uint64("numUnits", uint64(opts.NumUnits), "number of units")
+	flag.Parse()
+
+	opts.NumUnits = uint32(*numUnits) // workaround the missing type support for uint32
+}
+
+func processFlags() error {
+	if opts.ComputeProviderID < 0 {
+		return errors.New("-provider flag is required")
+	}
+
+	if commitmentAtxIdHex == "" {
+		return errors.New("-commitmentAtxId flag is required")
+	}
+	var err error
+	commitmentAtxId, err = hex.DecodeString(commitmentAtxIdHex)
+	if err != nil {
+		return fmt.Errorf("invalid commitmentAtxId: %w", err)
+	}
+
+	if idHex == "" {
+		pub, priv, err := ed25519.GenerateKey(nil)
+		if err != nil {
+			return fmt.Errorf("failed to generate identity: %w", err)
+		}
+		id = pub
+		log.Info("cli: generated id: %x", id)
+		saveKey(priv) // The key will need to be loaded in clients for the PoST data to be usable.
+	} else {
+		var err error
+		id, err = hex.DecodeString(idHex)
+		if err != nil {
+			return fmt.Errorf("invalid id: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	parseFlags()
+
+	if printProviders {
+		spew.Dump(gpu.Providers())
+		return
+	}
+
+	if printConfig {
+		spew.Dump(cfg)
+		spew.Dump(opts)
+		return
+	}
+
+	if err := processFlags(); err != nil {
+		log.Panic("cli: %v", err)
+	}
+
+	init, err := initialization.NewInitializer(
+		initialization.WithConfig(cfg),
+		initialization.WithInitOpts(opts),
+		initialization.WithNodeId(id),
+		initialization.WithCommitmentAtxId(commitmentAtxId),
+		initialization.WithLogger(log),
+	)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	if reset {
+		if err := init.Reset(); err != nil {
+			log.Panic("reset error: %v", err)
+		}
+		log.Info("cli: reset completed")
+		return
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	err = init.Initialize(ctx)
+	switch {
+	case errors.Is(err, shared.ErrInitCompleted):
+		log.Panic(err.Error())
+		return
+	case errors.Is(err, context.Canceled):
+		log.Info("cli: initialization interrupted")
+		return
+	case err != nil:
+		log.Error("cli: initialization error: %v", err)
+		return
+	}
+
+	log.Info("cli: initialization completed")
+
+	if genProof {
+		log.Info("cli: generating proof as a sanity test")
+
+		prover, err := proving.NewProver(cfg, opts.DataDir, id, commitmentAtxId)
+		if err != nil {
+			log.Panic(err.Error())
+		}
+		prover.SetLogger(log)
+		proof, proofMetadata, err := prover.GenerateProof(shared.ZeroChallenge)
+		if err != nil {
+			log.Panic("proof generation error: %v", err)
+		}
+		if err := verifying.Verify(proof, proofMetadata); err != nil {
+			log.Panic("failed to verify test proof: %v", err)
+		}
+
+		log.Info("cli: proof is valid")
+	}
+}
+
+func saveKey(key []byte) error {
+	if err := os.MkdirAll(opts.DataDir, shared.OwnerReadWriteExec); err != nil && !os.IsExist(err) {
+		return fmt.Errorf("mkdir error: %w", err)
+	}
+
+	if err := os.WriteFile(filepath.Join(opts.DataDir, "key.bin"), key, shared.OwnerReadWrite); err != nil {
+		return fmt.Errorf("key write to disk error: %w", err)
+	}
+	return nil
+}
+
+type logger struct{}
+
+func (l logger) Info(msg string, args ...any)    { baseLog.Printf("\tINFO\t"+msg, args...) }
+func (l logger) Debug(msg string, args ...any)   { baseLog.Printf("\tDEBUG\t"+msg, args...) }
+func (l logger) Warning(msg string, args ...any) { baseLog.Printf("\tWARN\t"+msg, args...) }
+func (l logger) Error(msg string, args ...any)   { baseLog.Printf("\tERROR\t"+msg, args...) }
+func (l logger) Panic(msg string, args ...any)   { baseLog.Fatalf(msg, args...) }
--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module github.com/spacemeshos/post
 go 1.19
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/minio/sha256-simd v1.0.0
 	github.com/spacemeshos/bitstream v0.1.0
+	github.com/spacemeshos/ed25519 v0.1.1
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/sync v0.1.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/klauspost/cpuid/v2 v2.1.1 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
