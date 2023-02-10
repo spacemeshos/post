45af45e6b83aec5eaa1f37411c5d3d4b59ba1d33
Matthias <5011972+fasmat@users.noreply.github.com>
2023-02-10 18:42:13 +0000

    Add documentation to config

--- config/config.go
+++ config/config.go
@@ -47,8 +47,8 @@ type Config struct {
 	MaxNumUnits   uint32
 	BitsPerLabel  uint8
 	LabelsPerUnit uint64
-	K1            uint32
-	K2            uint32
+	K1            uint32 // K1 specifies the difficulty for a label to be a candidate for a proof.
+	K2            uint32 // K2 is the number of labels below the required difficulty required for a proof.
 }
 
 func DefaultConfig() Config {
