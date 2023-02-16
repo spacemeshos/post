839c50ff5a325b7ed3e4c785e48a52354f3ab7b2
Matthias <5011972+fasmat@users.noreply.github.com>
2023-02-16 16:56:47 +0000

    Fix typos

--- shared/proof.go
+++ shared/proof.go
@@ -16,7 +16,7 @@ type ProofMetadata struct {
 
 	K1 uint32 // K1 specifies the difficulty for a label to be a candidate for a proof.
 	K2 uint32 // K2 is the number of labels below the required difficulty required for a proof.
-	B  uint32 // B is the number of labels used per AES invocation ven generating a proof.
+	B  uint32 // B is the number of labels used per AES invocation when generating a proof.
 	N  uint32 // N is the number of nonces tried at the same time when generating a proof.
 }
 
