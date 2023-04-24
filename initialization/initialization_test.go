2e0377cf46742ccebd871e31a23a7c36bad14270
Piers Powlesland <pierspowlesland@gmail.com>
2023-04-24 14:39:42 +0100

    Change validation to just be > 0
    Removed outdated doc specifying that the compute batch size must be
    divisible by 8. Updated returned validation error and test to not check
    for divisibility by 8.

--- initialization/initialization_test.go
+++ initialization/initialization_test.go
@@ -849,28 +849,6 @@ func TestValidateComputeBatchSize(t *testing.T) {
 		WithLogger(testLogger{t: t}),
 	)
 	assert.Error(t, err)
-
-	// Set invalid value of 4 (batch sizes must be divisible by 8)
-	opts.ComputeBatchSize = 4
-	_, err = NewInitializer(
-		WithNodeId(nodeId),
-		WithCommitmentAtxId(commitmentAtxId),
-		WithConfig(cfg),
-		WithInitOpts(opts),
-		WithLogger(testLogger{t: t}),
-	)
-	assert.Error(t, err)
-
-	// Set invalid value of 13 (batch sizes must be divisible by 8)
-	opts.ComputeBatchSize = 13
-	_, err = NewInitializer(
-		WithNodeId(nodeId),
-		WithCommitmentAtxId(commitmentAtxId),
-		WithConfig(cfg),
-		WithInitOpts(opts),
-		WithLogger(testLogger{t: t}),
-	)
-	assert.Error(t, err)
 }
 
 func assertNumLabelsWritten(ctx context.Context, t *testing.T, init *Initializer) func() error {
