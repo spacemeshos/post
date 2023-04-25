d0224a3a36d183fdb05c23702c62f4adb60df1f6
Matthias <5011972+fasmat@users.noreply.github.com>
2023-04-25 15:35:26 +0000

    Fix panicing test

--- internal/postrs/bridge_test.go
+++ internal/postrs/bridge_test.go
@@ -1,11 +1,13 @@
 package postrs
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/spacemeshos/post/config"
+	"github.com/spacemeshos/post/internal/gpu"
 )
 
 func TestTranslateScryptParams(t *testing.T) {
@@ -29,8 +31,23 @@ func TestGetProviders(t *testing.T) {
 }
 
 func TestInitialize(t *testing.T) {
-	t.Skip("skipping test - panics if no GPU is available")
-
 	err := Initialize()
 	require.NoError(t, err)
+
+	commitment := make([]byte, 32)
+	salt := make([]byte, 32)
+	providers := gpu.Providers()
+
+	res, err := gpu.ScryptPositions(
+		gpu.WithComputeProviderID(providers[0].ID),
+		gpu.WithCommitment(commitment),
+		gpu.WithSalt(salt),
+		gpu.WithStartAndEndPosition(1, 2),
+		gpu.WithBitsPerLabel(128), // 16 byte labels
+		gpu.WithScryptParams(8192, 1, 1),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.NotNil(t, res.Output)
+	fmt.Printf("res.Output: %x\n", res.Output)
 }
