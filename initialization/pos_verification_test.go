06d7a975cbaba765b689e953d9c3a52dcb146d7a
Bartosz Różański <bartek.roza@gmail.com>
2023-07-17 20:30:10 +0200

    Fix UT

--- initialization/pos_verification_test.go
+++ initialization/pos_verification_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestVerifyPos(t *testing.T) {
-	r := require.New(t)
+	t.Parallel()
 	cfg := config.DefaultConfig()
 	cfg.LabelsPerUnit = 128
 
@@ -32,28 +32,29 @@ func TestVerifyPos(t *testing.T) {
 		WithInitOpts(opts),
 		WithLogger(zaptest.NewLogger(t, zaptest.Level(zap.DebugLevel))),
 	)
-	r.NoError(err)
+	require.NoError(t, err)
 	err = init.Initialize(context.Background())
-	r.NoError(err)
+	require.NoError(t, err)
 
 	scryptParams := postrs.TranslateScryptParams(opts.Scrypt.N, opts.Scrypt.R, opts.Scrypt.P)
 
 	t.Run("valid", func(t *testing.T) {
-		err = postrs.VerifyPos(opts.DataDir, scryptParams, postrs.WithFraction(100.0))
-		r.NoError(err)
+		err := postrs.VerifyPos(opts.DataDir, scryptParams, postrs.WithFraction(100.0))
+		require.NoError(t, err)
 	})
 	t.Run("invalid N", func(t *testing.T) {
 		wrongScrypt := postrs.TranslateScryptParams(4, opts.Scrypt.R, opts.Scrypt.P)
-		err = postrs.VerifyPos(opts.DataDir, wrongScrypt, postrs.WithFraction(100.0))
-		r.ErrorContains(err, "invalid POS")
+		err := postrs.VerifyPos(opts.DataDir, wrongScrypt, postrs.WithFraction(100.0))
+		require.ErrorContains(t, err, "invalid POS")
 	})
 	t.Run("corrupted data", func(t *testing.T) {
 		file, err := os.OpenFile(opts.DataDir+"/postdata_0.bin", os.O_WRONLY, 0)
-		r.NoError(err)
+		require.NoError(t, err)
+		defer file.Close()
 		_, err = file.WriteAt([]byte("1234567890123456"), 0)
-		r.NoError(err)
+		require.NoError(t, err)
 
 		err = postrs.VerifyPos(opts.DataDir, scryptParams, postrs.WithFraction(100.0))
-		r.ErrorContains(err, "invalid POS")
+		require.ErrorContains(t, err, "invalid POS")
 	})
 }
